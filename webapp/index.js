9d188202f49b4ab8791dca7fc6816095f87028bd
hrku <chinum.upadhyaya@gmail.com>
2023-10-23 16:48:43 +0530

    UI5 Training , Step 3 Commit

---
+++ webapp/index.js
@@ -0,0 +1,10 @@
+sap.ui.define(["sap/m/Text"], function (Text) {
+  // We are using SAP/M/library here and in that we using text
+  "use strict";
+
+  new Text({
+    // Here we create that text object
+    text: "Hello UI5 using text control",
+  }).placeAt("content");
+});
+// Ending Step 3 Commit Here
